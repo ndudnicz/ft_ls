a0c86cd04ab3343b68ba3d2b51c35448829832ef
ndudnicz <ndudnicz@protonmail.com>
2018-04-06 17:51:14 +0200

    add some comment in entry.c

--- src/entry_list.c
+++ src/entry_list.c
@@ -4,6 +4,9 @@
 #include "libftasm.h"
 #include "error.h"
 
+/*
+** Init the new entry
+*/
 static t_entry		*init_entry(
 	t_entry *new,
 	t_u64 const length,
@@ -12,16 +15,17 @@ static t_entry		*init_entry(
 )
 {
 	new->length = length + 1;
+	new->mode |= S_ISDIR(*s) ? MODE_IS_NODE : 0;
 	ft_memcpy((void*)&new->stat, (void*)s, sizeof(struct stat));
 	if (!(new->name = ft_strdup(dir->d_name)))
 		return (NULL);
 	else
-	{
-		// printf("%d: %s\n", new->length, new->name);
 		return (new);
-	}
 }
 
+/*
+** Return a fresh 0ed entry
+*/
 static t_entry		*new_entry(void)
 {
 	t_u64 const	s = sizeof(t_entry);
@@ -29,6 +33,9 @@ static t_entry		*new_entry(void)
 	return ((t_entry*)ft_memset((void*)malloc(s), 0, s));
 }
 
+/*
+** Create a new entry, init it and return it
+*/
 t_entry				*create_entry(
 	t_u64 const length,
 	struct stat *s,
@@ -46,6 +53,9 @@ t_entry				*create_entry(
 		return (new);
 }
 
+/*
+** Push back the new entry at the end of the entry list
+*/
 t_entry		*push_back_entry(
 	t_entry **begin,
 	t_entry **new
